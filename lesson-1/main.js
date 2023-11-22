053e979dc614098ff5d0b24d26c9c11bfc7849df
ivan-andriichak <picea.abies10@gmail.com>
2023-11-22 11:31:45 +0200

    Add lesson-1

---
+++ lesson-1/main.js
@@ -0,0 +1,63 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+// let greetings = 'hello';
+// console.log(greetings);
+// let name = 'owu';
+// console.log(name);
+// let link = 'ua';
+// console.log(link);
+// let n1 = 1;
+// console.log(n1);
+// let n2 = 10;
+// console.log(n2);
+// let n3 = -999;
+// console.log(n3);
+// let n4 = 123;
+// console.log(n4);
+// const PI = 3.14;
+// console.log(PI);
+// const AS = 2.7;
+// console.log(AS);
+// let n5 = 16;
+// console.log(n5);
+// let variable = true;
+// console.log(variable);
+// let myVariable = false;
+// console.log(myVariable);
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstName = 'Ivan';
+// let middleName = 'Vasilovich';
+// let lastName = 'Andriichak';
+// // let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person);
+
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+// let person = firstName.concat(middleName).concat(lastName);
+// console.log(person);
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+//
+// firstName = prompt('your first name');
+// middleName = prompt('your middle name');
+// lastName = prompt('your last name');
+
+
+
+
