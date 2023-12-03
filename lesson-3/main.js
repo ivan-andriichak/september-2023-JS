6d447d870165ca8b23ca6436a058933f036ece1d
ivan-andriichak <picea.abies10@gmail.com>
2023-12-03 19:48:28 +0200

    Add lesson-3

---
+++ lesson-3/main.js
@@ -0,0 +1,119 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+// for (let i = 1; i <= 10; i++) {
+//     document.write(`<div>block № ${i}</div>`);
+// }
+// _________________________________________________________________________________________________
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+// let block = 1;
+// while (block <= 20) {
+//     document.write(`<div>block № ${block}</div>`);
+//     block += 1;
+// }
+// _________________________________________________________________________________________________
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+//
+    let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!--
+//             і тд інші об'єкти масиву
+//              ...
+//              ...
+//              ...
+//         -->
+//     </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+//     document.write(`<ul>`);
+// for (const listOfItem of listOfItems) {
+//     document.write(`<li>${listOfItem}</li>`);
+// }
+//     document.write(`</ul>`);
+// -----------------------------------------------
+//     Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+//
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+//     },
+//     {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+//     },
+//     {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+//     },
+//     {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+//     },
+// ];
+//
+// document.write(`<div class="product-card" style="width: 300px; height: 300px; margin: 0 auto">`);
+//
+// for (let i = 0; i < products.length; i++) {
+//    document.write(`<h3 class="product-title">${products[i].title}. Price ${products[i].price}</h3>
+//     <img src="${products[i].image}" alt="" class="product-image" style="width: 300px; height: 300px">
+//        `);
+//         }
+//
+//         document.write(`</div>`);
+
+// ШАБЛОН
+// <div class="product-card">
+//     <h3 class="product-title">TITLE. Price - PRICE</h3>
+// <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+// --------------------
+//     є масив
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+// let trueStatusUsers = [];
+// let falseStatusUsers = [];
+// let olderThan30Users = [];
+//
+// for (const user of users) {
+//     if (user.status === true) {
+//         trueStatusUsers.push(user);
+//     } else {
+//         falseStatusUsers.push(user);
+//     }
+//
+//     if (user.age > 30) {
+//         olderThan30Users.push(user);
+//     }
+// }
+//
+// console.log('Users with status true:', trueStatusUsers);
+// console.log('Users with status false:', falseStatusUsers);
+// console.log('Users older than 30:', olderThan30Users);
